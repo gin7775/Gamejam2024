f750f43d70bcd53311c8b18a2e6f926014af2c23
gin7775 <103499625+gin7775@users.noreply.github.com>
2024-01-26 23:02:08 +0100

    Update SpawnPollos.cs

--- Assets/Scripts/SpawnPollos.cs
+++ Assets/Scripts/SpawnPollos.cs
@@ -7,13 +7,16 @@ public class SpawnPollos : MonoBehaviour
     public GameObject [] pollosToSpawn;
     public Vector3 spawnPosition;
     public int randomIterastor;
-    public int enemyNumber;
+    public int enemyNumber,enemyCount;
+    public int waveNumber,currentWave,dificultiLevel;
+    
 
 
     // Start is called before the first frame update
     void Start()
     {
-        randomIterastor = Random.RandomRange(0, pollosToSpawn.Length);
+        randomIterastor = Random.Range(0, pollosToSpawn.Length);
+        
 
     }
 
@@ -24,15 +27,50 @@ public class SpawnPollos : MonoBehaviour
     }
 
 
-    public void InstantiatePollos()
+    public void InstantiatePollos(int enemys)
     {
-        for (int i = 0;i< enemyNumber;i++ )
+        enemyCount = enemys;
+        for (int i = 0;i< enemys;i++ )
         {
             randomIterastor = Random.Range(0, pollosToSpawn.Length);
-            spawnPosition = new Vector3(Random.Range(8.5f,-8.5f ), 0, Random.Range(8.5f, -8.5f));
+            spawnPosition = new Vector3(Random.Range(8.5f,-8.5f ), 1.2f, Random.Range(8.5f, -8.5f));
             GameObject toInstantiate = Instantiate(pollosToSpawn[randomIterastor], spawnPosition, Quaternion.identity);
         }
        
 
+    }
+    public void UpdateWave()
+    {
+        if (currentWave <= waveNumber)
+        {
+            if (currentWave <= waveNumber/3)
+            {
+                dificultiLevel = 1;
+                InstantiatePollos(enemyNumber);
+            }
+            else if (currentWave <= waveNumber / 2)
+            {
+                dificultiLevel=2;
+                InstantiatePollos(enemyNumber*2);
+            }
+            else 
+            {
+                dificultiLevel = 2;
+                InstantiatePollos(enemyNumber * 3);
+
+            }
+            currentWave++;
+            
+
+
+        }
+    }
+    public void enemyDeath()
+    {
+        enemyCount--;
+        if(enemyCount <= 0)
+        {
+            UpdateWave();
+        }
     }
 }
