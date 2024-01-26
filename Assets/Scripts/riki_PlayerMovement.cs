586ace4cb72a170f711e467659bfd552616bc5ab
Rarkdreik <tormentaoscura19@gmail.com>
2024-01-26 20:46:51 +0100

    test_riki

---
+++ Assets/Scripts/riki_PlayerMovement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class riki_PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public float rotationSpeed = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        Rotate();
+    }
+
+    private void Movement()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0, vertical) * speed * Time.deltaTime;
+        transform.Translate(movement);
+    }
+
+    private void Rotate()
+    {
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+        transform.Rotate(Vector3.up * rotation);
+    }
+
+}
