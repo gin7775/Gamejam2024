80a27b362d810ecf9426c656e57ce51864e59e89
merylol22 <merylol22@gmail.com>
2024-01-26 23:55:34 +0100

    Push ESCENAS

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void SceneGame(int sceneToChange)
+    {
+
+        switch (sceneToChange)
+        {
+            case 1:
+                SceneManager.LoadScene("Menu");
+                break;
+            case 2:
+                SceneManager.LoadScene("Opciones");
+                break;
+            case 3:
+                SceneManager.LoadScene("Credits");
+                break;
+            case 4:
+                Application.Quit();
+                break;
+
+        }
+
+    }
+
+
+
+    public void CargarNivel(string nombreNivel)
+    {
+        SceneManager.LoadScene(nombreNivel);
+    }
+
+}
+
+// Update is called once per frame
+void Update()
+    {
+        
+    }
+}
