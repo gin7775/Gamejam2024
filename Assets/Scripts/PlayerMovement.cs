2c761f8241ccc9198a8e352aa7573e3a7d0b3532
Rarkdreik <tormentaoscura19@gmail.com>
2024-01-31 23:51:25 +0100

    fix controler

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,18 +7,15 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
     public float rotationSpeed = 100f;
+    public float gravity = 9.81f; // Valor típico de gravedad, ajusta según necesites.
     public CharacterController controller;
 
     private Vector2 moveInput;
     private Vector2 rotateInput;
     private Animator animator;
     private Camera mainCamera;
-
-    public float gravity = 9.81f; // Valor típico de gravedad, ajusta según necesites.
     private Vector3 velocity;
-
     private GameManager gameManager;
-
 
     private void Awake()
     {
@@ -32,7 +29,6 @@ public class PlayerMovement : MonoBehaviour
         Rotate();
         Animations();
         ApplyGravity();
-       
     }
 
     public void Animations()
@@ -75,11 +71,25 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 desiredMoveDirection = (forward * moveInput.y + right * moveInput.x);
         controller.Move(desiredMoveDirection * speed * Time.deltaTime);
+
+        if (Gamepad.current != null && (moveInput.sqrMagnitude >= 0f && moveInput.sqrMagnitude <= 1f)) // Rotación con el joystick derecho
+        {
+
+            Vector3 direction = new Vector3(moveInput.x, 0f, moveInput.y);
+            RotateTowardsDirection(direction);
+            Debug.Log("Mueve");
+        }
     }
 
     private void Rotate()
     {
-        if (Mouse.current != null ) // Verificar si hay un mouse conectado
+        if (rotateInput.sqrMagnitude >= 0f && rotateInput.sqrMagnitude <= 1f) // Rotación con el joystick derecho
+        {
+            Vector3 direction = new Vector3(rotateInput.x, 0f, rotateInput.y);
+            RotateTowardsDirection(direction);
+            //Debug.Log("Mueve");
+        }
+        else // if (Mouse.current != null) // Verificar si hay un mouse conectado
         {
             Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
             if (Physics.Raycast(ray, out RaycastHit hitInfo))
@@ -94,14 +104,8 @@ public class PlayerMovement : MonoBehaviour
                 }
             }
         }
-        else if (rotateInput.sqrMagnitude > 0.01f) // Rotación con el joystick derecho
-        {
-            Vector3 direction = new Vector3(rotateInput.x, 0f, rotateInput.y);
-            RotateTowardsDirection(direction);
-            Debug.Log("Mueve");
-        }
-
     }
+
     private void RotateTowardsDirection(Vector3 direction)
     {
         if (direction.sqrMagnitude > 0.01f)
