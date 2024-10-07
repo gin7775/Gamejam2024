7b93a188afc5ee5df321f1280d0d70e0311539ec
AndresUFV <98354593+AndresUFV@users.noreply.github.com>
2024-10-08 00:43:12 +0200

    FeedBack + Invulnerabilidad del player cada vez que recibe daño
    Esta implementado en la escena de Richy y de Andres ( Faltarian algunas mejoras que terminaré mañana)

--- Assets/1_Scripts/ChickenLouncher.cs
+++ Assets/1_Scripts/ChickenLouncher.cs
@@ -1,10 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.ConstrainedExecution;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UIElements;
 using UnityEngine.InputSystem;
+using Cinemachine;
 
 
 public class ChickenLouncher : MonoBehaviour
@@ -43,12 +41,22 @@ public class ChickenLouncher : MonoBehaviour
     bool muriendo;
     [SerializeField] private bool gameModeFrenezzi = false;
 
+
+    [Header("RECIBIR DAÑO")]
+    [SerializeField] private float invulnerabilityDuration = 1f; 
+    private bool isInvulnerable = false;
+    [SerializeField] private ParticleSystem hitParticle;
     public GameObject RetryButton;
-
-
+    [SerializeField] private Material playerMaterial; 
+    private static readonly int IsColorShift = Shader.PropertyToID("_Is_ColorShift");
+    private static readonly int IsRimLight = Shader.PropertyToID("_Is_RimLight");
+    private static readonly int IsViewShift = Shader.PropertyToID("_Is_ViewShift");
+    private Coroutine invulnerabilityCoroutine;
+    CinemachineImpulseSource cinemachineImpulseSource;
     private void Start()
     {
         musicManager = FindAnyObjectByType<MusicManager>();
+        cinemachineImpulseSource = GetComponent<CinemachineImpulseSource>();
         muriendo = false;
         distanciaComparativa = 1000;
         anim = GetComponent<Animator>();
@@ -78,6 +86,7 @@ public class ChickenLouncher : MonoBehaviour
         //Debug.Log("Coge");
     }
 
+
     void RetrieveChicken(int chickenNumber)
     {
         anim.SetBool("Carrying", true);
@@ -103,20 +112,73 @@ public class ChickenLouncher : MonoBehaviour
 
     public void ReciveDamage(int damage)
     {
-        UpdateLifeUI();
-        health -= damage;
-        musicManager.Play_FX_PLayer_RecibirDaño();
-        if (health <= 0)
+        if (!isInvulnerable)
         {
-            PlayerDeath();
+
+            
+            StartCoroutine(InvulnerabilityCoroutine());
+            cinemachineImpulseSource.GenerateImpulse();
+            hitParticle.Play();
+            UpdateLifeUI();
+            health -= damage;
+           
+            musicManager.Play_FX_PLayer_RecibirDaño();
+
+            
+            if (health <= 0)
+            {
+                PlayerDeath();
+            }
         }
     }
+    
 
+   
+   
+    private IEnumerator InvulnerabilityCoroutine()
+    {
+        isInvulnerable = true;
+
+        // Activa el color shifting, rim light y view shift
+        playerMaterial.SetFloat(IsColorShift, 1);
+        playerMaterial.SetFloat(IsRimLight, 1);
+        playerMaterial.SetFloat(IsViewShift, 1);
+
+        // Tiempo total para la transición de desactivación
+        float duration = 1f; 
+        float elapsed = 0f; 
+
+        
+        yield return new WaitForSeconds(1f);
+
+        // Desactiva progresivamente el color shifting, rim light y view shift
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime; 
+
+            
+            float t = Mathf.Clamp01(elapsed / duration);
+
+            
+            playerMaterial.SetFloat(IsColorShift, 1 - t);
+            playerMaterial.SetFloat(IsRimLight, 1 - t);
+            playerMaterial.SetFloat(IsViewShift, 1 - t);
+
+            yield return null; 
+        }
+
+        
+        playerMaterial.SetFloat(IsColorShift, 0);
+        playerMaterial.SetFloat(IsRimLight, 0);
+        playerMaterial.SetFloat(IsViewShift, 0);
+
+        isInvulnerable = false;
+    }
     private void UpdateLifeUI()
     {
         if (eggs != null)
         {
-            eggs[health - 1].GetComponent<Animator>().SetTrigger("Break");
+            eggs[health - 1].GetComponent<Animator>().SetTrigger("Break");  
         }
     }
 
@@ -124,7 +186,17 @@ public class ChickenLouncher : MonoBehaviour
     {
         if(!muriendo) 
         {
-            //AQUI DEBERÍAN IR PARTICULAS DE PLUMAS TMB
+            hitParticle.Play();
+            if (invulnerabilityCoroutine != null) // Detén la coroutine si está corriendo
+            {
+                StopCoroutine(invulnerabilityCoroutine);
+                invulnerabilityCoroutine = null; // Reinicia la referencia
+            }
+
+            // Desactiva efectos de invulnerabilidad
+            playerMaterial.SetFloat(IsColorShift, 0);
+            playerMaterial.SetFloat(IsRimLight, 0);
+            playerMaterial.SetFloat(IsViewShift, 0);
             muriendo = true;
             StartCoroutine(TransicionMuerte());
         }
