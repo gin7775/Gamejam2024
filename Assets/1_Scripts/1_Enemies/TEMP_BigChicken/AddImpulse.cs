a83e150274b1010a36a7a1b44b16768f69f5ba1b
UnaiPepe <unai.pereztubia@gmail.com>
2024-10-24 18:36:50 +0200

    UPDATE Prototipos Pollo Gigante + Disparo

---
+++ Assets/1_Scripts/1_Enemies/TEMP_BigChicken/AddImpulse.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddImpulse : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float impulse = 5f;
+    public float drag = 0.1f;
+    public float angularDrag = 0.1f;
+
+    void Start()
+    {
+        rb = this.gameObject.GetComponent<Rigidbody>();
+        rb.drag = drag;
+        rb.angularDrag = drag;
+        AddForce();
+    }
+
+    public void AddForce()
+    {
+        // Aplicar impulso a la bola
+        rb.AddForce(transform.forward * impulse, ForceMode.Impulse);
+    }
+
+}
---
+++ Assets/1_Scripts/1_Enemies/TEMP_BigChicken/ProjectileBig.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileBig: MonoBehaviour
+{
+    [SerializeField] int Life = 5;
+    [SerializeField] int effectLife = 2;
+    [SerializeField] Collider[] colliders;
+    [SerializeField] GameObject[] ragdolls;
+
+    private bool isCodeExecuting = false;
+    public GameObject[] chickensToDie;
+    public float radius = 5f;
+
+    void Start()
+    {
+        SetObjectLifeTime();
+        StartCoroutine(SetRagdoll());
+    }
+
+    void SetObjectLifeTime()
+    {
+        StartCoroutine(ObjectEffectLife());
+    }
+
+    IEnumerator ObjectEffectLife()
+    {
+        yield return new WaitForSeconds(effectLife);
+
+        if(colliders != null)
+        {
+            foreach(Collider col in colliders)
+            {
+                col.enabled = false;
+            }
+        }
+
+        StartCoroutine(ObjectLife());
+    }
+
+    IEnumerator ObjectLife()
+    {
+        yield return new WaitForSeconds(Life);
+        Destroy(gameObject);
+    }
+
+    IEnumerator SetRagdoll()
+    {
+        yield return new WaitForSeconds(0.5f);
+        foreach(GameObject obj in ragdolls)
+        {
+            obj.SetActive(true);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Impacto");
+            DealDamage(other.gameObject, 2);
+        }
+    }
+
+    private void DealDamage(GameObject objetive, int damage)
+    {
+        //objetive.GetComponent<ContenedorEnemigo1>().ReciveDamage(damage);
+        GameManager.Instance.chickenEnemyTakeDamage(objetive, damage);
+    }
+
+   
+
+}
