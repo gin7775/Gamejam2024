d7f4680240de424c0cffcdab5097839bdce9d9d9
UnaiPepe <73905235+UnaiPepe@users.noreply.github.com>
2025-06-30 23:09:59 +0200

    Update SceneChangers.cs

--- Assets/1_Scripts/4_Utilidades/SceneChangers.cs
+++ Assets/1_Scripts/4_Utilidades/SceneChangers.cs
@@ -7,13 +7,6 @@ using UnityEngine.SceneManagement;
 
 public class SceneChangers : MonoBehaviour
 {
-    public string 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
